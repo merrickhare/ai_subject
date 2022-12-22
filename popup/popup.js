0d005df15d2a784af39cc97556de68f85f455f20
merrickhare <merrickhare@outlook.com>
2022-12-22 11:05:55 -0700

    Initial Project Commit

---
+++ popup/popup.js
@@ -0,0 +1,56 @@
+// Author: Merrick Hare
+// Date: 12/22/2022
+// Overrides the submit button and adds a bit of extra functionality to the reset button. 
+// The buttonSubmit function takes the information posted in the textarea and stores it as a prompt,
+// which is then sent to the completions enpoint. 
+
+// Store your api key from OpenAI as a string
+const API_KEY = "YOUR API KEY"
+
+// Get a reference to the form element
+var form = document.getElementById('submit_form');
+
+// Add a submit event listener to the form
+form.addEventListener('submit', function(event) {
+  // Prevent the form from submitting (default action)
+  event.preventDefault();
+
+  // Get the value of the input field
+  var inputValue = document.getElementById('emailBody').value;
+
+  // Pass the input value to the buttonSubmit function 
+  buttonSubmit(inputValue);
+});
+
+form.addEventListener('reset', function() {
+  document.getElementById("answer").innerHTML = "Paste your email body in the box below.";
+});
+// function that handles the request and response to the open ai endpoint
+function buttonSubmit(promptText){
+      fetch("https://api.openai.com/v1/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${API_KEY}`,
+        },
+        body: JSON.stringify({
+          "model": "text-davinci-003",
+          "prompt": `Create a subject line for an email from the following text: ${promptText}`,
+          "temperature": 0,
+          "max_tokens": 20
+        }),
+      })
+      // format the response in json
+      .then((response) => response.json())
+      // clear the answer element and populate with the returned subject
+      .then((data) => {
+           document.getElementById("answer").innerHTML = " "
+           document.getElementById("answer").innerHTML = "Subject Line: " + data['choices'][0]['text']
+      })
+      .catch((error) => {
+        document.getElementById("answer").innerHTML = " "
+        document.getElementById("answer").innerHTML = error;
+      });
+    };
+
+
---
+++ readme.md
@@ -0,0 +1,21 @@
+# AI Subject Line Creator
+Author: Merrick Hare \
+Version: 1.0 \
+Date: 12/22/2022
+
+### Description:
+Use this to create subject lines for your emails. It uses `fetch()` to send a payload to openAI completion endpoint. The response is returned and the text is displayed to the user. 
+
+# Requirements
+
+1. OpenAI account https://openai.com & API Key
+2. Chromium Browser (Edge or Chrome recommended)
+
+# Install 
+
+1. Clone this repository
+2. Open popup.js and replace "YOUR API KEY" with your API Key from OpenAI
+3. Open your browser and navigate to the extensions settings
+4. Enable Developer Mode
+5. Select "Load Unpacked"
+6. Navigate to the downloaded folder and select it
